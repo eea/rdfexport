b3b85e86c78323c4edb9133e56aa2766ba600f56
roug <roug@26ecc896-f4e2-0310-9f2f-ca50803180eb>
2013-05-04 11:33:50 +0000

    Refactoring: Made a generic escaper.

--- src/main/java/eionet/rdfexport/StringHelper.java
+++ src/main/java/eionet/rdfexport/StringHelper.java
@@ -1,8 +1,7 @@
 package eionet.rdfexport;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-//import org.apache.commons.lang.StringUtils;
+//import java.io.UnsupportedEncodingException;
+//import java.net.URLEncoder;
 
 /**
  * Class to help escape strings for XML and URI components.
@@ -15,10 +14,21 @@ final class StringHelper {
      * Characters that aren't allowed in IRIs. Special consideration for plus (+): It is historically used to encode space. If we
      * leave it unencoded, then it could be mistakenly decoded back to a space.
      */
-    private static final String[] BAD_IRI_CHARS = {" ", "{", "}", "<", ">", "\"", "|", "\\", "^", "`", "+"};
+    private static final char[] BAD_IRI_CHARS = {' ', '{', '}', '<', '>', '"', '|', '\\', '^', '`', '+'};
     /** Replacements for characters that aren't allowed in IRIs. */
-    private static final String[] BAD_IRI_CHARS_ESCAPES = {"%20", "%7B", "%7D", "%3C", "%3E", "%22", "%7C", "%5C", "%5E", "%60", "%2B"};
+    private static final String[] BAD_IRI_CHARS_ESCAPES = {"%20", "%7B", "%7D", "%3C", "%3E",
+                                                        "%22", "%7C", "%5C", "%5E", "%60", "%2B"};
 
+    /** * Characters that aren't allowed in XML.  */
+    private static final char[] BAD_XML_CHARS = {'\'', '"', '&', '<', '>'};
+    /** Replacements for characters that aren't allowed in XML. */
+    private static final String[] BAD_XML_CHARS_ESCAPES = {"&#39;", "&quot;", "&amp;", "&lt;", "&gt;"};
+
+    private static final char[] BAD_CMP_CHARS = {';', '/', '?', ':', '@', '&', '=',
+        '+', '$', ',', '[', ']', '<', '>', '#', '%', '\"', '{', '}', '\n', '\t', ' '};
+
+    private static final String[] BAD_CMP_CHARS_ESCAPES = {"%3B", "%2F", "%3F", "%3A", "%40", "%26", "%3D",
+        "%2B", "%24", "%2C", "%5B", "%5D", "%3C", "%3E", "%23", "%25", "%22", "%7B", "%7D", "%0A", "%09", "%20"};
     /**
      * Constructor. Since all methods are static we don't want instantiations of the class.
      */
@@ -34,6 +44,35 @@ final class StringHelper {
      * @return escaped string.
      */
     public static String escapeXml(String s) {
+        return escapeString(s, BAD_XML_CHARS, BAD_XML_CHARS_ESCAPES);
+    }
+
+    /**
+     * Escapes IRI's reserved characters in the given URL string.
+     *
+     * @param url
+     *            is a string.
+     * @return escaped URI
+     */
+    public static String encodeToIRI(String url) {
+        return escapeString(url, BAD_IRI_CHARS, BAD_IRI_CHARS_ESCAPES);
+    }
+
+    /**
+     * Escape characters that have special meaning.
+     *
+     * @param s
+     *            - The string to escape.
+     * @param badChars
+     *            - A list of the characters that are not allowed.
+     * @param escapeStrings
+     *            - A list of the strings to escape to.
+     * @return escaped string.
+     */
+    private static String escapeString(String s, char[] badChars, String[] escapeStrings) {
+        if (s == null) {
+            return s;
+        }
         int length = s.length();
         int newLength = length;
         // first check for characters that might
@@ -41,20 +80,11 @@ final class StringHelper {
         // of the string that has escapes.
         for (int i = 0; i < length; i++) {
             char c = s.charAt(i);
-            switch (c) {
-                case '\"':
-                    newLength += 5;
+            for (int badInx = 0; badInx < badChars.length; badInx++) {
+                if (c == badChars[badInx]) {
+                    newLength += escapeStrings[badInx].length() - 1;
                     break;
-                case '&':
-                case '\'':
-                    newLength += 4;
-                    break;
-                case '<':
-                case '>':
-                    newLength += 3;
-                    break;
-                default:
-                    break;
+                }
             }
         }
         if (length == newLength) {
@@ -62,26 +92,19 @@ final class StringHelper {
             return s;
         }
         StringBuffer sb = new StringBuffer(newLength);
+        boolean found;
         for (int i = 0; i < length; i++) {
             char c = s.charAt(i);
-            switch (c) {
-                case '\"':
-                    sb.append("&quot;");
+            found = false;
+            for (int badInx = 0; badInx < badChars.length; badInx++) {
+                if (c == badChars[badInx]) {
+                    sb.append(escapeStrings[badInx]);
+                    found = true;
                     break;
-                case '\'':
-                    sb.append("&#39;");
-                    break;
-                case '&':
-                    sb.append("&amp;");
-                    break;
-                case '<':
-                    sb.append("&lt;");
-                    break;
-                case '>':
-                    sb.append("&gt;");
-                    break;
-                default:
-                    sb.append(c);
+                }
+            }
+            if (!found) {
+                sb.append(c);
             }
         }
         return sb.toString();
@@ -101,25 +124,16 @@ final class StringHelper {
      * @return The escaped string.
      */
     public static String encodeURIComponent(String s, String enc) {
-        try {
-            return URLEncoder.encode(s, enc).replaceAll("\\+", "%20").replaceAll("\\%21", "!").replaceAll("\\%27", "'")
-                    .replaceAll("\\%28", "(").replaceAll("\\%29", ")").replaceAll("\\%7E", "~");
-        } catch (UnsupportedEncodingException e) {
-            // This exception should never occur.
-            return s;
-        }
+        return escapeString(s, BAD_CMP_CHARS, BAD_CMP_CHARS_ESCAPES);
     }
-
-//    /**
-//     * Escapes IRI's reserved characters in the given URL string.
-//     *
-//     * @param url
-//     *            is a string.
-//     * @return escaped URI
-//     */
-//    public static String encodeToIRI(String url) {
-//
-//        return (url == null) ? null : StringUtils.replaceEach(url, BAD_IRI_CHARS, BAD_IRI_CHARS_ESCAPES);
+//    public static String encodeURIComponent(String s, String enc) {
+//        try {
+//            return URLEncoder.encode(s, enc).replaceAll("\\+", "%20").replaceAll("\\%21", "!").replaceAll("\\%27", "'")
+//                    .replaceAll("\\%28", "(").replaceAll("\\%29", ")").replaceAll("\\%7E", "~");
+//        } catch (UnsupportedEncodingException e) {
+//            // This exception should never occur.
+//            return s;
+//        }
 //    }
 
 }
