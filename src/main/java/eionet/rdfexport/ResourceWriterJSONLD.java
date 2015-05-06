12a38c142d28b28b719b7c868dad2f5f3c6cf2fe
gsf-greece <gsf.greece@hotmail.com>
2015-05-06 13:55:30 +0300

    code quality improvements. Refs #21738

--- src/main/java/eionet/rdfexport/ResourceWriterJSONLD.java
+++ src/main/java/eionet/rdfexport/ResourceWriterJSONLD.java
@@ -29,7 +29,7 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
 
 /**
- *
+ * Class for writing a resource in JSON-LD. This class does not read properties files.
  * @author George Sofianos
  */
 public class ResourceWriterJSONLD {
@@ -52,17 +52,22 @@ public class ResourceWriterJSONLD {
     /** If output has started, then you can't change the nullNamespace. */
     private Boolean rdfHeaderWritten = false;
 
-    private final String JSONLD_CONTEXT = "@context";
-    private final String JSONLD_BASE = "@base";
-    private final String JSONLD_ID = "@id";
-    private final String JSONLD_LANGUAGE = "@language";
-    private final String JSONLD_TYPE = "@type";
-    private final String JSONLD_VALUE = "@value";
-    private final String JSONLD_VOCAB = "@vocab";
+    private static final String JSONLD_CONTEXT = "@context";
+    private static final String JSONLD_BASE = "@base";
+    private static final String JSONLD_ID = "@id";
+    private static final String JSONLD_LANGUAGE = "@language";
+    private static final String JSONLD_TYPE = "@type";
+    private static final String JSONLD_VALUE = "@value";
+    private static final String JSONLD_VOCAB = "@vocab";
 
     private JsonFactory f;
     private JsonGenerator json;
 
+    /**
+     * Constructor
+     * @param stream - the stream to write the output to
+     * @throws IOException
+     */
     public ResourceWriterJSONLD(OutputStreamWriter stream) throws IOException {
         outputStream = stream;
         namespaces = new HashMap<String, String>();
@@ -73,6 +78,10 @@ public class ResourceWriterJSONLD {
         json.useDefaultPrettyPrinter();
     }
 
+    /**
+     * Set the JSON-LD vocabulary in case it needs to be different from the properties file.
+     * @param url
+     */
     public void setVocabulary(final String url) {
         if (!url.equals(nullNamespace) && rdfHeaderWritten) {
             throw new RuntimeException("Can't set vocabulary after output has started!");
@@ -80,6 +89,10 @@ public class ResourceWriterJSONLD {
         nullNamespace = url;
     }
 
+    /**
+     * Generate the JSON-LD header element.
+     * @throws IOException
+     */
     void writeJsonLDHeader() throws IOException {
         if (rdfHeaderWritten) {
             return;
@@ -102,31 +115,64 @@ public class ResourceWriterJSONLD {
         json.writeEndObject();
     }
 
+    /**
+     * Set the base URL.
+     * @param url - the base URL
+     */
     public void setBaseURL(String url) {
         baseurl = url;
     }
 
+    /**
+     * Set a flag that tells how to deal with empty strings.
+     * @param emptyStringIsNull - flag. If true then don't output properties for empty strings.
+     */
     public void setEmptyStringIsNull(boolean emptyStringIsNull) {
         this.emptyStringIsNull = emptyStringIsNull;
     }
 
+    /**
+     * Add namespace to table.
+     * @param name - namespace token
+     * @param url - namespace url
+     */
     public void addNamespace(String name, String url) {
         namespaces.put(name, url);
     }
 
+    /**
+     * Generate the JSON-LD footer element.
+     * @throws IOException
+     */
     public void writeJsonLDFooter() throws IOException {
         json.writeEndObject();
         json.close();
     }
 
+    /**
+     * Generates a JSON array
+     * @param property - triple consisting of name, datatype and langcode
+     * @throws IOException
+     */
     void writeArray(RDFField property) throws IOException {
         json.writeArrayFieldStart(property.name);
     }
 
+    /**
+     * Ends a JSON array
+     * @throws IOException
+     */
     public void writeEndArray() throws IOException {
         json.writeEndArray();
     }
 
+    /**
+     * Write the start of a resource - the lines with @id and @type
+     * @param rdfClass - the class to assign
+     * @param segment - the namespace of the table
+     * @param id - the unqualified identifier of the resource
+     * @throws IOException
+     */
     public void writeStartResource(String rdfClass, String segment, String id) throws IOException {
         if (rdfHeaderWritten) {
             return;
@@ -146,6 +192,14 @@ public class ResourceWriterJSONLD {
         rdfHeaderWritten = true;
     }
 
+    /**
+     *
+     * @param property - triple consisting of name, datatype and langcode
+     * @param value - from database.
+     * @param isArray - if it has a parent array
+     * @throws SQLException - if the SQL database is not available
+     * @throws IOException - if the output is not open.
+     */
     void writeProperty(RDFField property, Object value, Boolean isArray) throws SQLException, IOException {
         if (value == null || (emptyStringIsNull && "".equals(value))) {
             return;
@@ -157,8 +211,7 @@ public class ResourceWriterJSONLD {
                 else json.writeStartObject();
                 json.writeStringField(JSONLD_ID, StringEncoder.encodeToXml(StringEncoder.encodeToIRI(Datatypes.getFormattedValue(value))));
                 json.writeEndObject();
-            }
-            else {
+            } else {
                 // Handle the case of ->countries or ->http://...
                 // If the ref-segment contains a colon then it can't be a fragment
                 // http://www.w3.org/TR/REC-xml-names/#NT-NCName
@@ -178,22 +231,28 @@ public class ResourceWriterJSONLD {
             json.writeStringField(JSONLD_TYPE, property.datatype);
             json.writeStringField(JSONLD_VALUE, StringEncoder.encodeToXml(Datatypes.getFormattedValue(value)));
             json.writeEndObject();
-        }
-        else if (!"".equals(property.langcode)) {
+        } else if (!"".equals(property.langcode)) {
             json.writeObjectFieldStart(property.name);
             json.writeStringField(JSONLD_LANGUAGE, property.langcode);
             json.writeStringField(JSONLD_VALUE, StringEncoder.encodeToXml(Datatypes.getFormattedValue(value)));
             json.writeEndObject();
-        }
-        else {
+        } else {
             json.writeStringField(property.name, StringEncoder.encodeToXml(Datatypes.getFormattedValue(value)));
         }
     }
 
+    /**
+     * Writes the end of a resource.
+     * @throws IOException
+     */
     public void writeEndResource() throws IOException {
         json.writeEndObject();
     }
 
+    /**
+     * Writes a JSON object start
+     * @throws IOException
+     */
     public void writeStartObject() throws IOException {
         json.writeStartObject();
     }
